f5119f0f6f5d197dce0b9e8a8028251a65e0c3e3
Jon Leithe <jon@jonleithe.no>
2022-11-14 20:34:47 +0100

    Copied out printUsage to own file

---
+++ src/printUsage.h
@@ -0,0 +1,17 @@
+/*
+ * printUsage.h
+ *
+ *	Created on: 13th of November 2022
+ *	Author: jole
+ */
+
+
+
+#ifndef SRC_PRINTUSAGE_H_
+#define SRC_PRINTUSAGE_H_
+
+#include <iostream>
+
+void printUsage(void);
+
+#endif /* SRC_PRINTUSAGE_H_ */
